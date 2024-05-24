957b11ba92590cb083d39853210cb1ca6074193d
yliu <yliu@rg3hpc1.ira.uka.de>
2024-05-24 21:03:29 +0200

    create emphasis webpage

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 async function loadFileList() {
-    const response = await fetch('/project/tts/students/yining_ws/multi_lng/TTS/data/emph_fake_audio/emph_filelist.txt');
+    const response = await fetch('resources/audio/emphTTS/en/vitsprosody030520241049PM/emph_filelist.txt');
     const data = await response.text();
     const rows = data.split('\n').filter(row => row.trim() !== '');
 
     const tableBody = document.getElementById('table-body');
 
     rows.forEach(row => {
-        const [filePath, description] = row.split('|').map(item => item.trim());'
+        const [filePath, description] = row.split('|').map(item => item.trim());
         
+        filePath = filePath.replace('/project/tts/students/yining_ws/multi_lng/TTS/data/emph_fake_audio/', 'resources/audio/emphTTS/en/vitsprosody030520241049PM/')
         // Debugging: Print the filePath and description
         console.log('File Path:', filePath);
         console.log('Description:', description);
